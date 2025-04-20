bb4212923b8076477b962ce6cc99e59809d4b304
awankelam777 <hitori.kiri7xj@gmail.com>
2025-04-21 00:11:28 +0700

    main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.getenv("7851637498:AAEcswBnyx55_rRM2YeKqZBhL7RhWvs7oTg")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
@@ -16,11 +16,11 @@ async def handle_file(update: Update, context: ContextTypes.DEFAULT_TYPE):
         return
 
     file_info = await file.get_file()
-    direct_link = f"https://api.telegram.org/file/bot{BOT_TOKEN}/{file_info.file_path}"
+    direct_link = f"https://api.telegram.org/file/bot{7851637498:AAEcswBnyx55_rRM2YeKqZBhL7RhWvs7oTg}/{file_info.file_path}"
     await update.message.reply_text(f"Nih link buat download langsung:\n{direct_link}")
 
 if __name__ == '__main__':
-    app = ApplicationBuilder().token(BOT_TOKEN).build()
+    app = ApplicationBuilder().token(7851637498:AAEcswBnyx55_rRM2YeKqZBhL7RhWvs7oTg).build()
     app.add_handler(CommandHandler('start', start))
     app.add_handler(MessageHandler(
         filters.Document.ALL | filters.PHOTO | filters.VIDEO | filters.AUDIO, handle_file
