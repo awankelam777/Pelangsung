4179e687e0b851cbf0f8403461e3d0ce1b8d6dcf
awankelam777 <hitori.kiri7xj@gmail.com>
2025-04-21 10:29:00 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
-
 import telebot
+from flask import Flask
+from threading import Thread
 
-BOT_TOKEN = "7851637498:AAEcswBnyx55_rRM2YeKqZBhL7RhWvs7oTg"
-bot = telebot.TeleBot(BOT_TOKEN)
+# Ganti ini dengan token bot kamu
+API_TOKEN = 'PASTE_TOKEN_KAMU_DI_SINI'
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.send_message(message.chat.id, "Halo! Bot Telegram kamu sudah aktif.")
+bot = telebot.TeleBot(API_TOKEN)
 
-bot.polling()
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, "Hai! Aku hidup di Glitch sekarang.")
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is running!"
+
+def run():
+    app.run(host='0.0.0.0', port=3000)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
+keep_alive()
+bot.infinity_polling()
