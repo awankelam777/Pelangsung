3a309cfc5424eba42a1e3ac5afa75a77c642c903
awankelam777 <hitori.kiri7xj@gmail.com>
2025-04-21 13:29:56 +0700

    Update main.py

--- main.py
+++ main.py
@@ -27,6 +27,20 @@ def run():
 def keep_alive():
     t = Thread(target=run)
     t.start()
+from flask import Flask
+from threading import Thread
 
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is running!"
+
+def run():
+    app.run(host='0.0.0.0', port=3000)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
 keep_alive()
 bot.infinity_polling()
